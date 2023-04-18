afc3864e509798467ea0198915e68dc8cdc20877
kE3kZ <keekli@gmail.com>
2023-04-18 15:59:04 -0500

    update 4/18 ak

---
+++ schemas.js
@@ -0,0 +1,14 @@
+const orderSchema = {
+    OrderId: '',
+    CustID: '',
+    EmpID: '',
+    BreadID: '',
+    BreadPriceID: '',
+    OrderTotal: ''
+  };
+
+  const customerSchema ={
+    CustID: '',
+    CustName: '',
+    CustContact: ''
+  }
